5828b368a51f11c6408f239409f908943ddf244e
ljp2 <ljp2@lehigh.edu>
2022-03-28 21:44:14 -0400

    working

--- gui.py
+++ gui.py
@@ -20,60 +20,62 @@ class GUI(Tk):
     def nextBar(self):
         bar = self.bars.get_next_bar()
         self.bars_queue.put(bar)
+
+    def init_vars(self):
+        self.filename = StringVar(value='20220202.csv')
+        self.position = StringVar(value=0)
+        self.avgprice = StringVar(value = 0)
+        self.unreal_profit_loss = StringVar(value = 0)
+        self.day_profit_loss = StringVar(value = 0)
+
             
     def __init__(self, bars_queue):
         super().__init__()
+        self.init_vars()
+
         self.title("Trading Simulation")
         self.bars_queue = bars_queue
 
-        mainframe = ttk.Labelframe(self)
+        mainframe = ttk.Frame(self)
         mainframe.grid(row=0, column=0, padx=10, pady=10)
 
-        filename_lbl = ttk.Label(mainframe,  text='Select Day File')
-        filename_lbl.grid(row=0, column=0)
-        self.filename = StringVar(value='20220202.csv')
+        filenameframe = ttk.Frame(mainframe)
+        filenameframe.grid(row=0,column=0)
+        select_file_entry = ttk.Entry(filenameframe, width=20, textvariable=self.filename)
+        select_file_entry.grid(row=0, column=0, padx=5, pady=5)
+        self.select_file_btn = ttk.Button(filenameframe, text='Open File', command=self.openFile)
+        self.select_file_btn.grid(row=0, column=1, padx=5, pady=5)
+        next_bar_btn = ttk.Button(filenameframe, text='Next Bar',  command=self.nextBar)
+        next_bar_btn.grid(row=0, column=2)
 
 
-        self.select_file_btn = ttk.Button(mainframe, text="Open File", command=self.openFile)
-        self.select_file_btn.grid(row=1, column=1, padx=5, pady=5)
-        select_file_entry = ttk.Entry(mainframe, width=20, textvariable=self.filename)
-        select_file_entry.grid(row=1, column=0)
-
-        next_bar_btn = ttk.Button(mainframe, text='Next Bar',  command=self.nextBar)
-        next_bar_btn.grid(row=1, column=2)
-
-        self.position = StringVar(value=0)
-        self.avgprice = StringVar(value = 0)
-        self.profit_loss = StringVar(value = 0)
-        self.current_profit_loss = StringVar(value = 0)
-        self.position_value = 0
-
-        positionframe = ttk.LabelFrame(mainframe, text = "Positions")
-        positionframe.grid(row=2, column=0, padx=10, pady=10)
+        positionframe = ttk.Frame(mainframe)
+        positionframe.grid(row=1, column=0, padx=10, pady=10)
         position_lbl = ttk.Label(positionframe, text="Position")
-        position_lbl.grid(row=0, column=0, padx=10, pady=10)
+        position_lbl.grid(row=0, column=0, padx=5, pady=5)
         self.position_entry = ttk.Entry(positionframe, width=8, textvariable=self.position)
         self.position_entry.grid(row=0, column=1, padx=10, pady=10)
 
         avgprice_lbl = ttk.Label(positionframe, text="AvgPrice")
         avgprice_lbl.grid(row=0, column=2, padx=10, pady=10)
         avgprice_entry = ttk.Entry(positionframe, width=8, textvariable=self.avgprice)
-        avgprice_entry.grid(row=0, column=3, padx=10, pady=10)
+        avgprice_entry.grid(row=0, column=3, padx=5, pady=5)
 
-        pl_lbl = ttk.Label(positionframe, text="Day Prof/Loss")
-        pl_lbl.grid(row=0, column=4, padx=10, pady=10)
-        pl_entry = ttk.Entry(positionframe, width=8, textvariable=self.profit_loss)
-        pl_entry.grid(row=0, column=5, padx=10, pady=10)
+        unreal_pl_lbl = ttk.Label(positionframe, text="UnReal P/L")
+        unreal_pl_lbl.grid(row=0, column=4, padx=10, pady=10)
+        unreal_pl_entry = ttk.Entry(positionframe, width=8, textvariable=self.unreal_profit_loss)
+        unreal_pl_entry.grid(row=0, column=5, padx=5, pady=5)
 
-        curpl_lbl = ttk.Label(positionframe, text="Current Prof/Loss")
-        curpl_lbl.grid(row=1, column=2, padx=10, pady=10)
-        curpl_entry = ttk.Entry(positionframe, width=8, textvariable=self.current_profit_loss)
-        curpl_entry.grid(row=1, column=3, padx=10, pady=10)
+        day_pl_lbl = ttk.Label(positionframe, text="Day P/L")
+        day_pl_lbl.grid(row=0, column=6, padx=10, pady=10)
+        day_pl_entry = ttk.Entry(positionframe, width=8, textvariable=self.day_profit_loss)
+        day_pl_entry.grid(row=0, column=7, padx=5, pady=5)
 
         buy_btn = ttk.Button(positionframe, text="Buy", command=self.buy)
         buy_btn.grid(row=1, column=0)        
         sell_btn = ttk.Button(positionframe, text="Sell", command=self.sell)
         sell_btn.grid(row=1, column=1)
+
 
     def buy(self):
         pos = int(self.position.get())
